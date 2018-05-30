9f2bdf33c61a762a99c50a7230145c33df731e74
Artem Maliuga <amwyomg@gmail.com>
2018-05-30 15:28:18 +0300

    add factorial with cycle

--- src/main/java/tasks/FactorialAndRecurse.java
+++ src/main/java/tasks/FactorialAndRecurse.java
@@ -2,14 +2,23 @@ package tasks;
 
 public class FactorialAndRecurse {
     public static void main(String[] args) {
-        System.out.println(factorialAndRecurse(3));
+        System.out.println(factorialWithRecurse(7));
+        System.out.println(factorialWithCycle(0));
     }
 
-    public static int factorialAndRecurse(int value) {
+    private static int factorialWithRecurse(int value) {
         if ((value == 1) || (value == 0)) {
             return 1;
         }
-        return factorialAndRecurse(value - 1) * value;
+        return factorialWithRecurse(value - 1) * value;
+    }
+
+    private static int factorialWithCycle(int value){
+        int temp = 1;
+        for (int i = 1; i <= value; i++) {
+            temp = i * temp;
+        }
+        return temp;
     }
 }
 
